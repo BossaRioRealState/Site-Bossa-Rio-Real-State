d62b26f39fd7f1e297d96141a0f2b7abacf10d95
Guilherme Buss <guibuss10@gmail.com>
2024-02-06 20:05:22 -0300

    Comecei o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,35 @@
 import Center from "src/components/Center";
 
-
 export default function Header(){
     return (
-        <div>
+        <div
+            style={{
+                position: "absolute",
+                width: "100%",
+                zIndex: 100,
+            }}
+        >
             <Center>
                 <header
                     className="header"
-                >
-
-                <h1>Header</h1>
-                <p>Áreas</p>
-                <p>Contato</p>
-
+                    style={{
+                        color: "#045570",
+                    }}
+                    >
+                    <div className="opcoes_menu">
+                        <div className="nav">
+                            <ul>
+                                <li className="opcao_header"><a href="/">Home</a></li>
+                                <li className="opcao_header"><a href="/imoveis">Imóveis</a></li>
+                                <li className="opcao_header"><a href="/compradores">Compradores</a></li>
+                                <li className="opcao_header"><a href="/vendas">Vendas</a></li>
+                                <li className="opcao_header"><a href="/sobre_nos">Sobre nós</a></li>
+                                <li className="opcao_header"><a href="/Contatos">Portifólio</a></li>
+                                <li className="opcao_header"><a href="/idioma">Idioma</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </header>
-
             </Center>
         </div>
     )
