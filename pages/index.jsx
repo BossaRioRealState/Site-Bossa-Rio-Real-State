fd34cb0a5e5d6b0fe5fcce8ebdb3fdab67d0ea7e
Guilherme Buss <guibuss10@gmail.com>
2024-02-07 22:33:02 -0300

    Comecei o Footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Header from "src/components/Header"
+import Footer from "src/components/Footer"
 import Center from "src/components/Center"
 import Texto from "src/components/Texto";
 import Backgroundimage from "src/components/Backgroundimage"
@@ -350,6 +351,7 @@ export default function Home() {
                 </Texto>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,34 @@
 import Center from "src/components/Center"
+import Texto from "src/components/Texto"
+
 
 export default function Footer() {
     return (
-        <div className="Footer">
+        <div className="Footer" style={{display:"grid"}}>
             <Center>
                 <footer>
-                    Footer
+                        <div className="footer_esquerda">
+                            <div style={{display:"grid"}}>
+                                <div>
+                                    <img
+                                        src="/images/logo.png"
+                                        alt="Logo Bossa rio"
+                                        className="img-center"
+                                        height="100px"
+                                        style={{ marginTop: '-5%'}}
+                                    />
+                                </div>
+                                <div style={{marginTop:"-10%",marginLeft:"15%"}}>
+                                    <Texto>
+                                        <p>Your Modern Office Space<br></br> for more productibility</p>
+                                    </Texto>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="footer_direita" style={{top:0,left:"50%",}}>
+
+                        </div>
                 </footer>
             </Center>
         </div>
