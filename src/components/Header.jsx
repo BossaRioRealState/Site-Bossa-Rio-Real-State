17f7eb3d4de5be894890c5415fddbeb41c92775f
Guilherme Buss <guibuss10@gmail.com>
2024-02-10 17:41:14 -0300

    Fiz a base do dropdown de idiomas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 import Center from "src/components/Center";
+import { useState } from "react";
 
 export default function Header(){
+    const [IdiomMenuActive, setIdiomMenuActive] = useState(false);
+
+    const toggleIdiomMenu = () => {
+        setIdiomMenuActive(!IdiomMenuActive);
+    };
+
     return (
         <div
             style={{
@@ -24,8 +31,31 @@ export default function Header(){
                                 <li className="opcao_header"><a href="/clientes">Clientes</a></li>
                                 <li className="opcao_header"><a href="/vendas">Vendas</a></li>
                                 <li className="opcao_header"><a href="/sobre_nos">Sobre nós</a></li>
-                                <li className="opcao_header"><a href="/Contatos">Portifólio</a></li>
-                                <li className="opcao_header"><a href="/idioma">Idioma</a></li>
+                                <li className="opcao_header"><a href="/Contatos">Contatos</a></li>
+                                <li className="opcao_header" onClick={toggleIdiomMenu}>Idioma
+                                    <div className="dropdown">
+                                        <div className={`nav-idiom ${IdiomMenuActive ? 'active' : ''}`}>
+                                            <ul>
+                                                <li><a href="/">Português (PT-BR)</a>
+                                                    <img    
+                                                        src="/images/header/brasil_flag.png"
+                                                        className="img-brasil"
+                                                        width="60px"
+                                                        style={{display: "block", zIndex: "3", }}
+                                                    />
+                                                </li>
+                                                <li><a href="/english/home">English (ENG)</a>
+                                                    <img    
+                                                        src="/images/header/eua_flag.png"
+                                                        className="img-brasil"
+                                                        width="60px"
+                                                        style={{display: "block", zIndex: "3", }}
+                                                    />
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
                             </ul>
                         </div>
                     </div>
