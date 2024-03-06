b860ea4bff7c6b32119cd3372add16aeeff1a908
Guilherme Buss <guibuss10@gmail.com>
2024-03-06 11:50:33 -0300

    Corrigi vários fatores do footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,87 +4,61 @@ import Texto from "src/components/Texto"
 
 export default function Footer() {
     return (
-        <div className="Footer" style={{ display: "grid" }} id="footer">
-            <Center>
-                <footer>
-                    <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
-                        <div className="footer_esquerda" style={{ marginTop: "-8%" }}>
-                            <div>
-                                <div>
-                                    <img
-                                        src="/images/logo.png"
-                                        alt="Logo Bossa rio"
-                                        className="img-center"
-                                        height="100px"
-                                        style={{ marginTop: '-5%' }}
-                                    />
-                                </div>
-                                <div style={{ marginTop: "-15%", marginLeft: "30%" }}>
-                                    <Texto>
-                                        <p>Your Modern Office Space<br></br> for more productibility</p>
-                                    </Texto>
-                                </div>
-                            </div>
-                        </div>
+        <footer id="footer">
+            <div className="footer_esquerda">
+                <img
+                    src="/images/logo.png"
+                    alt="Logo Bossa rio"
+                    className="logo_footer"
+                />
+                <Texto>
+                    <p>Your Modern Office Space<br></br> for more productibility</p>
+                </Texto>
+            </div>
 
-                        <div className="footer_direita" style={{ display: "grid", gridGap: "80px", gridTemplateColumns: "repeat(3, 1fr)", marginTop: "-13%", marginLeft: "15%" }}>
-                            <div>
-                                <Texto>
-                                    <h4>Vendas</h4>
-                                    <p4><a href="/imoveis">Imóveis</a></p4>
-                                    <p4><a href="/clientes">Clientes</a></p4>
-                                </Texto>
-                            </div>
-                            <div>
-                                <Texto>
-                                    <h4>Veja também</h4>
-                                    <p4><a href="/sobre_nos">Sobre Nós</a></p4>
-                                    {/* <p4><a href="/"></a></p4> */}
-                                </Texto>
-                            </div>
-                            <div>
-                                <Texto>
-                                    <h4>Contatos</h4>
-                                    <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", }}>
-                                        <a href="https://wa.me/5521971878889"><img
-                                            src="/images/footer/whatsapp.png"
-                                            alt="whatsapp"
-                                            className="img-center"
-                                            width="70%"
-                                            style={{
-                                            }}
-                                        /></a>
-                                        <a href="https://www.instagram.com/bossariorealestate"><img
-                                            src="/images/footer/instagram.png"
-                                            alt="instagram"
-                                            className="img-center"
-                                            width="70%"
-                                            style={{
-                                            }}
-                                        /></a>
-                                        <a href="mailto:bossario@plantelturismo.com.br "><img
-                                            src="/images/footer/email.png"
-                                            alt="email"
-                                            className="img-center"
-                                            width="70%"
-                                            style={{
-                                            }}
-                                        /></a>
-                                    </div>
-                                </Texto>
-                            </div>
-                        </div>
-                        {/* <div style={{marginTop: "80px", display: "flex"}}>
-                            <Texto>
-                                <h4>Número: </h4>
-                            </Texto>
-                            <Texto>
-                                <p4> <a href="https://wa.me/5521971878889"> +55 (21) 97187-8889</a></p4>
-                            </Texto>
-                        </div> */}
+            <div className="footer_direita">
+                <div>
+                    <Texto>
+                        <h4>Vendas</h4>
+                        <p4><a href="/imoveis">Imóveis</a></p4>
+                        <p4><a href="/clientes">Clientes</a></p4>
+                    </Texto>
+                </div>
+                <div>
+                    <Texto>
+                        <h4>Veja também</h4>
+                        <p4><a href="/sobre_nos">Sobre Nós</a></p4>
+                    </Texto>
+                </div>
+                <div>
+                    <Texto>
+                        <h4>Contatos</h4>
+                    </Texto>
+                    <div className="container_contatos">
+                        <a href="https://wa.me/5521971878889">
+                            <img
+                                src="/images/footer/whatsapp.png"
+                                alt="whatsapp"
+                                className="logo_contato"
+                            />
+                        </a>
+                        <a href="https://www.instagram.com/bossariorealestate">
+                            <img
+                                src="/images/footer/instagram.png"
+                                alt="instagram"
+                                className="logo_contato"
+                            />
+                        </a>
+                        <a href="mailto:bossario@plantelturismo.com.br">
+                            <img
+                                src="/images/footer/email.png"
+                                alt="email"
+                                className="logo_contato"
+                            />
+                        </a>
                     </div>
-                </footer>
-            </Center>
-        </div>
+                </div>
+            </div>
+        </footer>
     )
 }
