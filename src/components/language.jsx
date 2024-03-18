fa116e7aac974acec06cba278bb5424b874b0470
Guilherme Buss <guibuss10@gmail.com>
2024-03-18 11:29:32 -0300

    Agora o idioma permanece ao mudar a página.

--- src/components/language.jsx
+++ src/components/language.jsx
@@ -1,19 +1,31 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 // Create a context
 const LanguageContext = createContext();
 
 // Create a provider that holds the state and functions
 export function LanguageProvider({ children }) {
-  const [currentLanguage, setCurrentLanguage] = useState(0);
+  const [currentLanguage, setCurrentLanguage] = useState(
+    typeof window !== 'undefined' ? localStorage.getItem('currentLanguage') || 0 : 0
+  );
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('currentLanguage', currentLanguage);
+    }
+  }, [currentLanguage]);
 
   const MudarIdiomaPort = () => setCurrentLanguage(0);
   const MudarIdiomaEng = () => setCurrentLanguage(1);
 
   return (
-    <LanguageContext.Provider value={{ currentLanguage, MudarIdiomaPort, MudarIdiomaEng }}>
-      {children}
-    </LanguageContext.Provider>
+    isClient && (
+      <LanguageContext.Provider value={{ currentLanguage, MudarIdiomaPort, MudarIdiomaEng }}>
+        {children}
+      </LanguageContext.Provider>
+    )
   );
 }
 
